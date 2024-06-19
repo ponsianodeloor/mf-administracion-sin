c1ca8be275674dedeb15718eb909648774ac4f9b
Kevin Mauricio Muenala Olmedo <Kevin.Muenala@fj.local>
2024-06-19 12:10:12 -0500

    Base micro frontends

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './modules/test/pages/test/test.component';
 const routes: Routes = [
 ];
 @NgModule({
