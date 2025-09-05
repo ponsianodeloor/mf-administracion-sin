91ab48bd07409ab776aeb025e27c57917116656a
ponsianodeloor <ponsianodeloor@gmail.com>
2025-09-05 15:47:48 -0500

    formulario para parametros

--- src/app/features/parametros/components/modals/create-or-update-parametros-facturacion-notarias-modal/create-or-update-parametros-facturacion-notarias-modal.component.ts
+++ src/app/features/parametros/components/modals/create-or-update-parametros-facturacion-notarias-modal/create-or-update-parametros-facturacion-notarias-modal.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ShellMaterialModule} from "../../../../../shared/modules/shell-material.module";
 
 @Component({
   selector: 'app-create-or-update-parametros-facturacion-notarias-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    ShellMaterialModule
+  ],
   templateUrl: './create-or-update-parametros-facturacion-notarias-modal.component.html',
   styleUrl: './create-or-update-parametros-facturacion-notarias-modal.component.scss'
 })
